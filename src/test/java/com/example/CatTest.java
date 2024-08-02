3012911de21ccd29410bdcdaba95846db2826de5
Grigoriy Ryzhkov <groTex22@yandex.ru>
2024-08-02 03:48:48 +0300

    sprint_6_1

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,31 +1,24 @@
 package com.example;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-
 import java.util.Arrays;
 import java.util.List;
 
-/*В этом тестовом классе замокали класс Feline
-* Возможно в контексте этого кода и не совсем нужно, но для тренировки*/
 @RunWith(MockitoJUnitRunner.class)
 public class CatTest {
-    Cat cat;
+    //Мок класса от которого зависит класс Cat
     @Mock
     Feline feline;
 
-    @Before
-    public void beforeTest() {
-        cat = new Cat(feline);
-    }
     //Будем проверять как говорит котик
     @Test
     public void getSoundReturnCatSound(){
+        Cat cat = new Cat(feline);
         Assert.assertEquals("Мяу", cat.getSound());
     }
 
@@ -33,9 +26,10 @@ public class CatTest {
     //Так же проверим, что он кушает
     @Test
     public void getFoodEatMeat() throws Exception{
-
+        Cat cat = new Cat(feline);
         List<String> expectedResult = Arrays.asList("Животные", "Птицы", "Рыба");
-        Mockito.when(feline.eatMeat()).thenReturn(expectedResult);
+
+        Mockito.when(feline.eatMeat()).thenReturn(Arrays.asList("Животные", "Птицы", "Рыба"));
         Assert.assertEquals(expectedResult, cat.getFood());
     }
 
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import static org.hamcrest.CoreMatchers.any;
-import static org.hamcrest.CoreMatchers.containsString;
 import org.hamcrest.MatcherAssert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,14 +7,15 @@ import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import java.util.Arrays;
 import java.util.List;
+import static org.hamcrest.CoreMatchers.containsString;
 
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+    @Mock
+    Feline feline;
 
-public class TestLion  {
-    Feline feline = new Feline();
     Lion lion ;
 
 
@@ -27,47 +26,36 @@ public class TestLion  {
     }
 
 
-
-
     //Проверяем создание объекта с несуществуещем полом животного
     //перехватываем ошибку не с помощью аннотации, а с помощью try catch
     //чтобы сравнить текст ошибки
     @Test
     public void createLionWrongParemetr() {
-        String lerror = "Используйте допустимые значения пола животного - самей или самка";
+        String err = "Используйте допустимые значения пола животного - самей или самка";
         try {
         lion = new Lion("Пацан",feline);
         //А вдруг создасться, поэтому пишем ниже fail, чтобы исключить ложную отработку
         Assert.fail("Лев не должен был быть создан. Если создали, то ай яй яй");
         } catch (Exception exception) {
             //Сверим текст исключения с ожидаемым
-            MatcherAssert.assertThat(exception.getMessage(), containsString(lerror));
+            MatcherAssert.assertThat(exception.getMessage(), containsString(err));
         }
     }
 
     //В этом тесте проверим сколько возвращается 1 экземпляр льва
     @Test
-    public void getKittensReturnOneKittens() {
+    public void getKittensReturnOneCount() {
+        Mockito.when(feline.getKittens()).thenReturn(1);
         Assert.assertEquals(1, lion.getKittens());
     }
 
     //Проверим что кушает лев
     @Test
-    public void getFoodPredatorreturnMeat() throws Exception {
-        List<String> expectedResult = Arrays.asList("Животные", "Птицы", "Рыба");
+    public void getFoodPredatorEat() throws Exception {
+        List<String> expectedResult = List.of("Животные", "Птицы", "Рыба");
+
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         Assert.assertEquals(expectedResult, lion.getFood());
     }
 
-    //Проверим, что если лев самец, то вернется true
-    @Test
-    public void doesHaveManeMaleHasManeTrue() {
-        Assert.assertTrue("Получаем TRUE, если лев самец", lion.doesHaveMane());
-    }
-
-    //Проверим, что если лев самка, то вернется false
-    @Test
-    public void doesHaveManeFemaleHasManeFalse() throws Exception {
-        lion = new Lion("Самка",feline);
-        Assert.assertFalse("Получаем False, если это львица", lion.doesHaveMane());
-    }
 }
